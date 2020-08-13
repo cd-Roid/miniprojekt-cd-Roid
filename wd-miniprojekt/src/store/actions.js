42a110b0eb97dccb2b619c7513bb09ff4cc0018c
cd-Roid <giyawe98@gmail.com>
2020-08-14 00:32:44 +0200

    data is now being fetched and stored by language

--- wd-miniprojekt/src/store/actions.js
+++ wd-miniprojekt/src/store/actions.js
@@ -2,12 +2,15 @@ import german from "../../json/cda-paintings-v2.de.json";
 import english from "../../json/cda-paintings-v2.en.json";
 
 export default {
-  fetchDataGerman({ commit }) {
-    const data = german.items;
-    commit("fetchDataGerman", data);
-  },
-  fetchDataEnglish({ commit }) {
-    const data = english.items;
-    commit("fetchDataEnglish", data);
+  groupByYear({ commit }, lang) {
+    if (lang === "en") {
+      const data = english.items;
+      commit("group", data);
+    } else if (lang === "de") {
+      const data = german.items;
+      commit("group", data);
+    } else {
+      console.error("no language specified!");
+    }
   }
 };
--- wd-miniprojekt/src/store/mutations.js
+++ wd-miniprojekt/src/store/mutations.js
@@ -1,8 +1,14 @@
 export default {
-  fetchDataGerman(state, data) {
-    state.data_de = data;
-  },
-  fetchDataEnglish(state, data) {
-    state.data_en = data;
+
+  group(state, data) {
+    state.chunks = [];
+    const arr = [];
+    for (let index = 1520; index <= 1620; index++) {
+      let element = data.filter(items => items.dating.begin == index);
+      arr[index] = { element };
+    }
+    const filtered = arr.filter(items => items.element != 0);
+    console.log(filtered[0])
+    state.chunks = filtered;
   }
 };
