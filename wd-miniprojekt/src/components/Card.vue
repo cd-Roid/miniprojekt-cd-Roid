e7e27579e4b6709622a07ed06ec79e5ed4508b3d
cd-Roid <giyawe98@gmail.com>
2020-08-14 00:32:07 +0200

    simple card component

---
+++ wd-miniprojekt/src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img :src="realUrl" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    url: Object
+  },
+  data() {
+    return {
+      realUrl: this.url.sizes.s.src
+    };
+  }
+};
+</script>
+
+<style></style>
