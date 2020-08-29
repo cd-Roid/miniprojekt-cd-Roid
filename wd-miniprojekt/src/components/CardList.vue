e27d665a77d8f57eef4347b8dcd96b9432ce0066
cd-Roid <giyawe98@gmail.com>
2020-08-29 20:28:19 +0200

    Refactored styles

--- wd-miniprojekt/src/components/CardList.vue
+++ wd-miniprojekt/src/components/CardList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div v-for="article in articles" :key="article.objectId">
+  <div class="card">
+    <div v-for="article in articles" :key="article.objectId" class="card">
       <Card :article="article" />
     </div>
   </div>
@@ -18,3 +18,27 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+@import "../styles/_variables.scss";
+.card {
+  grid-column: span 2;
+  &__img {
+    width: $desktop-image-size;
+    height: $desktop-image-size;
+    object-fit: cover;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+@media (max-width: 768px) {
+  .card {
+    grid-column: span 1;
+    margin: 0;
+    &__img {
+      object-fit: contain;
+      width: 100%;
+    }
+  }
+}
+</style>
