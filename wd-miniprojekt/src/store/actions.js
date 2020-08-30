90d84bf858767aaa515e0bd8aec77cb0ac8d49fd
cd-Roid <giyawe98@gmail.com>
2020-08-30 18:43:37 +0200

    added root "."  to fetch link

--- wd-miniprojekt/src/store/actions.js
+++ wd-miniprojekt/src/store/actions.js
@@ -1,14 +1,14 @@
 export default {
   async setData({ commit }, lang) {
     if (lang === "English") {
-      const res = await fetch("/data/json/cda-paintings-v2.en.json");
+      const res = await fetch("./data/json/cda-paintings-v2.en.json");
       const data = await res.json();
       const langList = [{ id: 1, lang: "Deutsch" }];
       commit("mutateData", { data, lang });
       commit("destructure", data);
       commit("setLangList", langList);
     } else if (lang === "Deutsch") {
-      const res = await fetch("/data/json/cda-paintings-v2.de.json");
+      const res = await fetch("./data/json/cda-paintings-v2.de.json");
       const data = await res.json();
       const langList = [{ id: 1, lang: "English" }];
       commit("mutateData", { data, lang });
